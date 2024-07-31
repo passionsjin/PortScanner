c199135ea3abc77e52462cf3060a5fc4b4549e17
passionsjin <dudwls251@naver.com>
2024-07-31 09:59:36 +0900

    output directory volume 설정

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -d --env-file .env --network host --name port_scanner port_scanner:latest
+docker run -d --env-file .env --network host \
+-v ./output:/app/output \
+--name port_scanner port_scanner:latest
